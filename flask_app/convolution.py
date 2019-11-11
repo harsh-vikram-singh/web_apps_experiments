b754edd0b0a8851a5a6aca4075ac87fb3713f63f
Harsh Vikram Singh <harsh.singh@renewbuy.com>
2019-11-11 19:23:22 +0530

    changes in the front end, and added the functionality for file upload

--- flask_app/convolution.py
+++ flask_app/convolution.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import secure_filename
+# from pathlib import Path
 
 app = Flask(__name__)
 
@@ -9,9 +11,15 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/upload_data")
-def upload_data():
-    return render_template("upload_data.html")
+@app.route("/upload_file", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        # folder = Path("user_uploads")
+        f.save("user_uploads/" + secure_filename(f.filename))
+    return """
+    <h1>File uploaded. Backend not ready yet</h1>
+    """
 
 
 if __name__ == "__main__":
