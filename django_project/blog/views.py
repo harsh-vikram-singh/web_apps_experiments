f96c67860e162d61f28f351e89c03b47ca861e45
harsh-vikram-singh <singh.harshvikram@gmail.com>
2019-11-29 00:27:20 +0530

    added templates and static files

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Richard McClintock',
+        'title': 'Ancient Latin Literature Series - 1',
+        'content': '''Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis doloribus repellendus dignissimos voluptas
+        veritatis quia necessitatibus suscipit expedita. Quis recusandae aspernatur atque praesentium ipsum laudantium
+        doloribus aliquam mollitia saepe provident quos, veritatis dignissimos quas excepturi ullam quidem, quam quia iure
+        animi sint velit. Ipsum accusantium pariatur autem iusto commodi et aliquam modi ullam, reprehenderit suscipit eius
+        aspernatur doloremque alias repellendus iure id quae voluptatem obcaecati nesciunt, rem corporis vero hic nam?
+        Soluta deserunt quam quaerat non. Non totam soluta sit.''',
+        'date_posted': 'Near 45 BC'
+    },
+    {
+        'author': 'Richard McClintock',
+        'title': 'Ancient Latin Literature Series - 2',
+        'content': '''Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis doloribus repellendus dignissimos voluptas
+        veritatis quia necessitatibus suscipit expedita. Quis recusandae aspernatur atque praesentium ipsum laudantium
+        doloribus aliquam mollitia saepe provident quos, veritatis dignissimos quas excepturi ullam quidem, quam quia iure
+        animi sint velit. Ipsum accusantium pariatur autem iusto commodi et aliquam modi ullam, reprehenderit suscipit eius
+        aspernatur doloremque alias repellendus iure id quae voluptatem obcaecati nesciunt, rem corporis vero hic nam?
+        Soluta deserunt quam quaerat non. Non totam soluta sit.''',
+        'date_posted': 'Near 45 BC'
+    },
+
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'context_post': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
